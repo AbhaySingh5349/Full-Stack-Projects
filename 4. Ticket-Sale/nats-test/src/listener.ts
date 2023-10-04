572414d84cdb6960f0befa91f1b43ee3dcff7c76
AbhaySingh5349 <abhay5349singh@gmail.com>
2023-10-04 18:42:15 +0530

    added nats streaming for event handling

---
+++ 4. Ticket-Sale/nats-test/src/listener.ts
@@ -0,0 +1,26 @@
+import nats, { Message, Stan } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+import { TicketCreatedListener } from './events/ticket-created-listener';
+
+console.clear();
+
+const client = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
+
+client.on('connect', () => {
+  console.log('listener connected to NATS');
+
+  // when we try to close the client or discnnect from running server
+  client.on('close', () => {
+    console.log('NATS connection closed');
+    process.exit();
+  });
+
+  new TicketCreatedListener(client).listen();
+});
+
+// watching for interupt and termination signals
+process.on('SIGINT', () => client.close());
+process.on('SIGTERM', () => client.close());
